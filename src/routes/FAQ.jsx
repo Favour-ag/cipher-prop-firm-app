691ba7833ece16f046e465a3abf43a81becb4833
Aghahowa Favour AIGBEKAEN <aigbekaenfavour@gmail.com>
2024-04-11 16:37:19 +0100

    About Created

--- src/routes/FAQ.jsx
+++ src/routes/FAQ.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const FAQ = () => {
-  return <div>FAQ</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="bg-[#010f1c] flex items-center justify-center w-full h-screen">
+        <p className="text-7xl"> Coming Soon...</p>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default FAQ;
