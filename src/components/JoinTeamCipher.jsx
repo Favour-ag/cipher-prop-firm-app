8f76011561dbee0f8c68e9883a2b52414393484d
Aghahowa Favour AIGBEKAEN <aigbekaenfavour@gmail.com>
2024-04-18 18:26:05 +0100

    commit

--- src/components/JoinTeamCipher.jsx
+++ src/components/JoinTeamCipher.jsx
@@ -19,7 +19,7 @@ const JoinTeamCipher = () => {
       {/* BUTTONS */}
       <div className="flex justify-start mt-6 lg:mt-0 space-x-3 lg:space-x-6 items-start text-sm md-text-base">
         <div className="py-3 px-0.5  bg-gradient-to-r from-red-500 to-blue-600 rounded-3xl">
-          <a href="#" className="py-3 px-3  bg-[#010f1c] rounded-3xl">
+          <a href="#" className="py-2.5 px-3  bg-[#010f1c] rounded-3xl">
             Select An Account
           </a>
         </div>
