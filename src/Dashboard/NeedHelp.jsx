d34a71d84f90f7a12c05cc77e8f4530d66df35ae
Aghahowa Favour AIGBEKAEN <aigbekaenfavour@gmail.com>
2024-05-10 16:16:33 +0100

    made adjustments

--- src/Dashboard/NeedHelp.jsx
+++ src/Dashboard/NeedHelp.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-import SideNav from "./SideNav";
-import AccountInfo from "../components/AccountInfo";
+import Contact from "../routes/Contact";
 
 const NeedHelp = () => {
   return (
-    <div className="flex bg-inherit w-full h-full">
-      <SideNav />
-      <div className="w-full h-screen p-3">
-        <AccountInfo />
-        <div className="flex flex-col w-full justify-center items-center h-full">
-          <p>Nothing here...</p>
-        </div>
-      </div>
-    </div>
+    <>
+      <Contact />
+    </>
   );
 };
 
