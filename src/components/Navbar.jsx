c00c9ef4e2127d5cf86fe8362a6af6986d7e07b0
Aghahowa Favour AIGBEKAEN <aigbekaenfavour@gmail.com>
2024-05-22 16:11:55 +0100

    Worked on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import logo from "../assets/cipher.png";
@@ -6,16 +6,17 @@ import { socials } from "../constants";
 
 const Navbar = () => {
   const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+  const location = useLocation();
 
   const toggleNavbar = () => {
     setMobileDrawerOpen(!mobileDrawerOpen);
   };
 
   return (
-    <nav className="sticky top-0 z-50 bg-[#010f1c] w-full ">
-      <div className="container px-2 md:px-3 py-2 md:py-3.5 lg:py-0  mx-auto relative text-sm lg:px-7">
+    <nav className="sticky top-0 z-50 bg-[#010f1c] w-full">
+      <div className="container px-2 md:px-3 py-2 md:py-3.5 lg:py-0 mx-auto relative text-sm lg:px-7">
         {/* DESKTOP VIEW */}
-        <div className="flex justify-between items-center lg:py-3 ">
+        <div className="flex justify-between items-center lg:py-3">
           {/* LOGO */}
           <div className="flex items-center flex-shrink-0">
             <a href="/">
@@ -27,77 +28,85 @@ const Navbar = () => {
             </a>
           </div>
           {/* NAVLIST */}
-          {/* <ul className="hidden lg:flex ml-14 space-x-6 text-base transition-all	">
-            <li className="hover:text-slate-300 ">
-              <Link to="/"> Home </Link>
-            </li>
-            <li className="hover:text-slate-300">
-              <Link to="/about"> About Us </Link>
-            </li>
-            <li className="hover:text-slate-300">
-              <Link to="/affliateprogram"> Affliate Program </Link>
-            </li>
-            <li className="hover:text-slate-300">
-              <Link to="/faq"> FAQ </Link>
-            </li>
-            <li className="hover:text-slate-300">
-              <Link to="/contact"> Contact </Link>
-            </li>
-          </ul> */}
-          <nav className=" hidden lg:flex ml-14 space-x-6 text-base transition-all">
-            <Link
-              to="/"
-              className={`text-white font-bold px-4 py-2 rounded ${
-                location.pathname === "/" ? "text-blue-500" : ""
-              }`}
-            >
-              Home
-            </Link>
-            <Link
-              to="/about"
-              className={`text-white font-bold px-4 py-2 rounded ${
-                location.pathname === "/about" ? "text-blue-500" : ""
-              }`}
-            >
-              About
-            </Link>
-            <Link
-              to="/affliateprogram"
-              className={`text-white font-bold px-4 py-2 rounded ${
-                location.pathname === "/affliateprogram" ? "text-blue-500" : ""
-              }`}
-            >
-              Affiliate Program
-            </Link>
-            <Link
-              to="/faq"
-              className={`text-white font-bold px-4 py-2 rounded ${
-                location.pathname === "/faq" ? "text-blue-500" : ""
-              }`}
-            >
-              FAQ
-            </Link>
-
-            <Link
-              to="/contact"
-              className={`text-white font-bold px-4 py-2 rounded ${
-                location.pathname === "/contact" ? "text-blue-500" : ""
-              }`}
-            >
-              Contact
-            </Link>
+          <nav className="hidden lg:flex ml:32 ">
+            <ul className="flex  text-base space-x-8 transition-all">
+              <li>
+                <Link
+                  to="/"
+                  onClick={toggleNavbar}
+                  className={`${
+                    location.pathname === "/"
+                      ? "text-pink-600 font-semibold"
+                      : "text-white"
+                  }`}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/about"
+                  onClick={toggleNavbar}
+                  className={`${
+                    location.pathname === "/about"
+                      ? "text-pink-600 font-semibold"
+                      : "text-white"
+                  }`}
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/affliateprogram"
+                  onClick={toggleNavbar}
+                  className={`${
+                    location.pathname === "/affliateprogram"
+                      ? "text-pink-600 font-semibold"
+                      : "text-white"
+                  }`}
+                >
+                  Affiliate Program
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/faq"
+                  onClick={toggleNavbar}
+                  className={`${
+                    location.pathname === "/faq"
+                      ? "text-pink-600 font-semibold"
+                      : "text-white"
+                  }`}
+                >
+                  FAQ
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/contact"
+                  onClick={toggleNavbar}
+                  className={`${
+                    location.pathname === "/contact"
+                      ? "text-pink-600 font-semibold"
+                      : "text-white"
+                  }`}
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
           </nav>
-
           <div className="flex flex-end gap-3">
             {/* BUTTONS */}
             <div className="hidden lg:flex justify-center space-x-6 items-center">
-              <p className="py-3 px-4   bg-gradient-to-r from-red-500 to-blue-500 rounded-3xl">
-                <Link to="/login"> Login/Register </Link>
+              <p className="py-3 px-4 bg-gradient-to-r from-red-500 to-blue-500 rounded-3xl">
+                <Link to="/login">Login/Register</Link>
               </p>
-              <div className="py-3 px-0.5  bg-gradient-to-r from-red-500 to-blue-500 rounded-3xl">
+              <div className="py-3 px-0.5 bg-gradient-to-r from-red-500 to-blue-500 rounded-3xl">
                 <a
                   href="#cipherchallenge"
-                  className="py-3 px-3  bg-[#010f1c] rounded-3xl"
+                  className="py-3 px-3 bg-[#010f1c] rounded-3xl"
                 >
                   Select An Account
                 </a>
@@ -105,16 +114,16 @@ const Navbar = () => {
             </div>
             {/* SELECT ACCOUNT BUTTON */}
             <div className="flex lg:hidden relative top-[-5px]">
-              <div className="py-2.5 px-0.5  bg-gradient-to-r from-red-500 to-blue-500 rounded-3xl">
+              <div className="py-2.5 px-0.5 bg-gradient-to-r from-red-500 to-blue-500 rounded-3xl">
                 <a
                   href="#cipherchallenge"
-                  className="py-2 px-3  bg-[#010f1c] rounded-3xl text-sm md:text-base"
+                  className="py-2 px-3 bg-[#010f1c] rounded-3xl text-sm md:text-base"
                 >
                   Select An Account
                 </a>
               </div>
             </div>
-            <div className="lg:hidden md:flex flex-col justify-center  top-0">
+            <div className="lg:hidden md:flex flex-col justify-center top-0">
               <button onClick={toggleNavbar}>
                 {mobileDrawerOpen ? <X size={35} /> : <Menu size={35} />}
               </button>
@@ -124,27 +133,77 @@ const Navbar = () => {
         {/* MOBILE MENU */}
         {mobileDrawerOpen && (
           <div className="fixed right-0 z-20 bg-[#010f1c] w-full h-screen flex flex-col items-center text-center lg:hidden gap-3 transition duration-700 ease-in">
-            <div className=" w-[400px] h-[400px] bg-[#023564] rounded-full absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] blur-[100px]"></div>
+            <div className="w-[400px] h-[400px] bg-[#023564] rounded-full absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] blur-[100px]"></div>
             <ul className="space-y-3 mt-8 text-lg z-10">
               <li>
-                <Link to="/"> Home </Link>
+                <Link
+                  to="/"
+                  onClick={toggleNavbar}
+                  className={`${
+                    location.pathname === "/"
+                      ? "text-pink-600 font-semibold"
+                      : "text-white"
+                  }`}
+                >
+                  Home
+                </Link>
               </li>
               <li>
-                <Link to="/about"> About Us </Link>
+                <Link
+                  to="/about"
+                  onClick={toggleNavbar}
+                  className={`${
+                    location.pathname === "/about"
+                      ? "text-pink-600 font-semibold"
+                      : "text-white"
+                  }`}
+                >
+                  About Us
+                </Link>
               </li>
               <li>
-                <Link to="/affliateprogram"> Affliate Program </Link>
+                <Link
+                  to="/affliateprogram"
+                  onClick={toggleNavbar}
+                  className={`${
+                    location.pathname === "/affliateprogram"
+                      ? "text-pink-600 font-semibold"
+                      : "text-white"
+                  }`}
+                >
+                  Affiliate Program
+                </Link>
               </li>
               <li>
-                <Link to="/faq"> FAQ </Link>
+                <Link
+                  to="/faq"
+                  onClick={toggleNavbar}
+                  className={`${
+                    location.pathname === "/faq"
+                      ? "text-pink-600 font-semibold"
+                      : "text-white"
+                  }`}
+                >
+                  FAQ
+                </Link>
               </li>
               <li>
-                <Link to="/contact"> Contact </Link>
+                <Link
+                  to="/contact"
+                  onClick={toggleNavbar}
+                  className={`${
+                    location.pathname === "/contact"
+                      ? "text-pink-600 font-semibold"
+                      : "text-white"
+                  }`}
+                >
+                  Contact
+                </Link>
               </li>
             </ul>
             <div className="z-10 space-y-1">
               <p className="text-lg z-10">Follow us on social media</p>
-              {/* SOCIAL MEDIA  */}
+              {/* SOCIAL MEDIA */}
               <div>
                 <ul className="flex items-center justify-center space-x-2.5">
                   {socials.map((social) => (
@@ -156,10 +215,10 @@ const Navbar = () => {
               </div>
             </div>
             <div className="flex flex-col text-center space-y-3 w-[75%] mt-8 z-10">
-              <p className="py-3 px-4   bg-gradient-to-r from-red-500 to-blue-500 text-lg">
+              <p className="py-3 px-4 bg-gradient-to-r from-red-500 to-blue-500 text-lg">
                 <a href="/login">Dashboard</a>
               </p>
-              <div className="py-0.5 px-0.5  bg-gradient-to-r from-red-500 to-blue-500">
+              <div className="py-0.5 px-0.5 bg-gradient-to-r from-red-500 to-blue-500">
                 <div className="py-2 px-0.5 bg-[#010f1c] text-lg">
                   <a href="#cipherchallenge">Select An Account</a>
                 </div>
